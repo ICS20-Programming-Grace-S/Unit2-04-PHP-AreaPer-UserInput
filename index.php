194b6352df44a3ae7d2d824e11b6ea4ca6e57371
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-29 00:38:16 +0000

    edit

--- index.php
+++ index.php
@@ -18,28 +18,49 @@
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
-    <title>Area of a Triangle in PHP</title>
+    <title>Area and Perimeter of a Triangle in PHP</title>
   </head>
   <body>
     
 		<!-- php echo to print the html to the page -->
     <?php 
-			echo "<h1>Area of a Triangle in PHP</h1>";
+			echo "<h1>Area and Perimeter of a Triangle in PHP</h1>";
 			echo "<center><h3> Calculate the area of a triangle.</h3><center>";
 		?>
+    <center><h3><?php echo "A = bxh/2.";?></h3><center>
+      
+      <center><h3><?php echo "Calculate the perimeter of a triangle.";?></h3><center>
+        <center><h3><?php echo "P = a+b+c.";?></h3><center>
 
 		<!-- form to get the base and height from the user -->
-    <form action="./triangle.php" method="post" target="results">
-      <label for=lblBase">Base (cm):</label>
+    <form action="./triangle.php" method="post" target="areaResults">
+      <label for="lblBase">Base (cm):</label>
       <input type="text" id="base" placeholder="Enter the base (cm)" name="base"><br><br>
       <label for="lblHeight">Height (cm):</label>
       <input type="text" id="height" placeholder="Enter the height (cm)" name="height"><br><br>
       <input type="submit" value="Calculate Area">
+      
 		</form>
-
+        
+      <!--form to the the base and two sides from user input.-->
+      <form action="./triangle.php" method="post" target="perimeterResults">
+        <label for="perBase">Base (cm):</label>
+        <input type="text" id="perimeterBase" placeholder="Enter the base (cm)" name="perBase"><br><br>
+        <label for=aSide">A Side (cm):</label>
+        <input type="text" id="perimeterASide" placeholder="Enter the A side (cm)" name="perASide"><br><br>
+        <label for=bSide">B Side (cm):</label>
+        <input type="text" id="perimeterBSide" placeholder="Enter the B side (cm)" name="perBSide"><br><br>
+        <input type="submit" value="Calculate Perimeter">
+      </form>
+        
 			<!-- iframe for the results to show on the web page. -->
-			<iframe id="results" name="results">			
+			<iframe id="areaPerResults" name="areaPerResults">			
 				The area of the triangle is  " + $area + cm<sup>2</sup>
+        
+        The perimeter of the triangle is  " + $perimeter + cm
 	    </iframe>
+
+        
+        
 	</body>
 </html>
--- triangle.php
+++ triangle.php
@@ -8,3 +8,16 @@
 ?>
 <h3>Area of Triangle:</h3>
 The area of the triangle is <?php echo "$area" ?>cm<sup>2</sup>.
+
+<?php
+
+// get the base and height from the textfields
+$perBase = $_POST['perimeterASide'];
+$perASide = $_POST['perimeterASide'];
+$perBSide = $_POST['perimeterBSide'];
+
+	// calculate the area
+	$perimeter = $perBase + $perAside + $perBside
+?>
+<h3>Area of Triangle:</h3>
+The perimeter of the triangle is <?php echo "$perimeter" ?>cm
